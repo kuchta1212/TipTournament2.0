f6e22940629549896cea8b551a951e7bb9aec75a
Jan Kuchar <jku@prodata.cz>
2021-06-09 11:15:13 +0200

    multiple selection on AllBets

--- TipTournament2.0/ClientApp/src/components/Bets/AllBets.tsx
+++ TipTournament2.0/ClientApp/src/components/Bets/AllBets.tsx
@@ -2,12 +2,19 @@
 import { getApi } from "../api/ApiFactory"
 import { User } from "../../typings/index"
 import { Bets } from './Bets';
+import { Loader } from './../Loader'
 import { UserSelector } from './UserSelector';
 import { WarningNotification, WarningTypes } from '../WarningNotification';
+import { Button } from 'bootstrap';
+import { Identifier } from 'typescript';
+
+interface IDictionary {
+    [key: number]: User
+}
 
 interface AllBetsState {
     users: User[];
-    selectedUser: User | undefined;
+    selectedUsers: IDictionary | undefined;
     loading: boolean;
     isUserSelected: boolean,
     isAllowed: boolean
@@ -24,10 +31,10 @@ export class AllBets extends React.Component<AllBetsProps, AllBetsState> {
 
         this.state = {
             users: {} as User[],
-            selectedUser: undefined,
+            selectedUsers: {0: {} as User },
             loading: true,
             isUserSelected: false,
-            isAllowed: new Date() > new Date("2021-06-11 21:00")
+            isAllowed: new Date() > new Date("2021-06-07 21:00")
         }
     }
 
@@ -37,7 +44,7 @@ export class AllBets extends React.Component<AllBetsProps, AllBetsState> {
 
     public render() {
         return this.state.loading
-            ? (<p><em>Loading...</em></p>)
+            ? (<Loader />)
             : !this.state.isUserSelected
                 ? this.renderUserSelector()
                 : this.renderAllBets()
@@ -45,30 +52,86 @@ export class AllBets extends React.Component<AllBetsProps, AllBetsState> {
 
     private async loadData() {
         const users = await getApi().getUsers();
-        this.setState({ users: users, selectedUser: undefined, loading: false, isUserSelected: false });
+        this.setState({ users: users, loading: false });
     }
 
     private renderUserSelector() {
         return this.state.isAllowed 
-            ? (<UserSelector users={this.state.users} onUserSelect={this.userSelected.bind(this)} disabled={false} />)
+            ? (
+                <React.Fragment>
+                    {this.renderUserSelectors()}
+                    <button className="btn btn-link" onClick={() => this.addUserSelector()}><img src={process.env.PUBLIC_URL + 'icons/add.svg'} width="25" height="25" /></button> 
+                </React.Fragment>)
             : (<React.Fragment>
-                <WarningNotification text="Sázky ostatních se otevřou s uzavřením sázek. Takže 11.6 v 21:00 " type={WarningTypes.error} />
-                <UserSelector users={this.state.users} onUserSelect={this.userSelected.bind(this)} disabled={true} />
+                    <WarningNotification text="Sázky ostatních se otevřou s uzavřením sázek. Takže 11.6 v 21:00 " type={WarningTypes.error} />
+                    {this.renderUserSelectors()}
+                    {/*<UserSelector id={0} users={this.state.users} onUserSelect={this.userSelected.bind(this)} value={'default'} disabled={true} />*/}
                 </React.Fragment>)
     }
 
     private renderAllBets() {
         return (
             <React.Fragment>
-                <UserSelector users={this.state.users} onUserSelect={this.userSelected.bind(this)} disabled={false} />
-                <Bets key={this.state.selectedUser.id} user={this.state.selectedUser} />
+                {this.renderUserSelectors()}    
+                <button className="btn btn-link" onClick={() => this.addUserSelector()}><img src={process.env.PUBLIC_URL + 'icons/add.svg'} width="25" height="25" /></button> 
+                <Bets key={this.getCombineId()} user={this.state.selectedUsers[0]} />
             </React.Fragment>
             );
     }
 
-    private userSelected(event: any) {
+    private userSelected(event: any, id: number) {
         let user = this.state.users.find(u => u.id == event.target.value);
-        this.setState({ users: this.state.users, selectedUser: user, loading: false, isUserSelected: true })
+        let selectedUsers = !!this.state.selectedUsers
+            ? this.state.selectedUsers
+            : {} as IDictionary;
+        selectedUsers[id] = user;
+        this.setState({ selectedUsers: selectedUsers, loading: false, isUserSelected: true })
+    }
+
+    private renderUserSelectors() {
+        console.log(this.state.selectedUsers);
+        return (<React.Fragment>
+            {Object.keys(this.state.selectedUsers).map((key, index) => {
+                return <UserSelector key={this.getKey(parseInt(key))} id={parseInt(key)} users={this.state.users} onUserSelect={this.userSelected.bind(this)} onSelectorRemove={this.removeUserSelector.bind(this)} value={!!this.state.selectedUsers[key].id ? this.state.selectedUsers[key].id : 'default'} disabled={false} />
+            })}
+        </React.Fragment>
+        )
+    }
+
+    private addUserSelector() {
+        let selectedUsers = this.state.selectedUsers;
+        const maxStr = Object.keys(selectedUsers).reduce((acc, cur) => {
+            let accInt = parseInt(acc);
+            let curInt = parseInt(cur);
+
+            return accInt > curInt
+                ? acc
+                : cur;
+        }, "-1")
+
+        const newIndex = parseInt(maxStr) + 1;
+        selectedUsers[newIndex] = {} as User;
+        this.setState({ selectedUsers: selectedUsers })
+    }
+
+    private removeUserSelector(event: any, id: number) {
+        console.log(id);
+        let selectedUsers = this.state.selectedUsers;
+        delete selectedUsers[id];
+
+        console.log(selectedUsers);
+
+        this.setState({ selectedUsers: selectedUsers });
+    }
+
+    private getCombineId(): string {
+        return Object.keys(this.state.selectedUsers).reduce((acc, cur) => {
+            return acc += this.state.selectedUsers[parseInt(cur)].id;
+        }, "CombineId-");
+    }
+
+    private getKey(id: number): string {
+        return "UserSelectorKey-" + id;
     }
 }
 
--- TipTournament2.0/ClientApp/src/components/Bets/UserSelector.tsx
+++ TipTournament2.0/ClientApp/src/components/Bets/UserSelector.tsx
@@ -5,9 +5,12 @@ interface UserSelectorState {
 }
 
 interface UserSelectorProps {
+    id: number,
     users: User[],
-    onUserSelect: (event: any) => void,
-    disabled: boolean
+    onUserSelect: (event: any, id: number) => void,
+    onSelectorRemove: (event: any, id: number) => void,
+    disabled: boolean,
+    value: string
 }
 
 export class UserSelector extends React.Component<UserSelectorProps, UserSelectorState> {
@@ -18,18 +21,28 @@ export class UserSelector extends React.Component<UserSelectorProps, UserSelecto
 
     public render() {
         return (
-            <div className="input-group mb-3">
-              <div className="input-group-prepend">
+            <div id={this.getId()} className="input-group mb-3">
+                <div className="input-group-prepend">
                     <label className="input-group-text" htmlFor="usersSelect">Vyber hráče:</label>
-              </div>
-                <select className="custom-select" id="usersSelect" defaultValue={'default'} onChange={this.props.onUserSelect} disabled={this.props.disabled}>
-                <option key="defaultOption" value="default" disabled>---</option> 
-                {this.props.users.map((user, index) => {
-                        return <option key={user.id} value={user.id}>{user.userName}</option>
-                })}
-              </select>
+                </div>
+                <select className="custom-select" id="usersSelect" defaultValue={this.props.value} onChange={(event) => this.props.onUserSelect(event, this.props.id)} disabled={this.props.disabled}>
+                    <option key="defaultOption" value="default" disabled>---</option> 
+                    {this.props.users.map((user, index) => {
+                            return <option key={user.id} value={user.id}>{user.userName}</option>
+                    })}
+                </select>
+                {
+                    this.props.id != 0
+                        ? <div className="input-group-append"><button className="btn btn-link" type="button" onClick={(event) => this.props.onSelectorRemove(event, this.props.id)}><img src={process.env.PUBLIC_URL + 'icons/close.svg'} width="20" height="20" /></button></div>
+                        : <div/>
+                }
+               
             </div>
         );
+    }
+
+    private getId(): string {
+        return "UserSelectorId-" + this.props.id;
     }
 }
 
