7885531b3e70bd11e9f648180effe94a918b54aa
Kuchta1212 <jku@prodata.cz>
2022-11-26 21:05:12 +0100

    adding new files

---
+++ ClientApp/src/components/Bets/GroupBetsView.tsx
@@ -0,0 +1,71 @@
+﻿import * as React from 'react';
+import { Match, Bet, Group, TournamentStage, User, GroupBet } from "../../typings/index"
+import { Table } from 'reactstrap';
+import { Loader } from '../Loader';
+import { getApi } from '../api/ApiFactory';
+import { GroupTable } from '../Bets/GroupTable';
+import { Dictionary, IDictionary } from '../../typings/Dictionary';
+
+interface GroupBetsViewState {
+    groups: IDictionary<IDictionary<GroupBet>>,
+    loading: boolean,
+}
+
+interface GroupBetsViewProps {
+    users: User[] | undefined
+}
+
+export class GroupBetsView extends React.Component<GroupBetsViewProps, GroupBetsViewState> {
+
+    constructor(props: GroupBetsViewProps) {
+        super(props);
+        this.state = {
+            groups: {} as IDictionary<IDictionary<GroupBet>>,
+            loading: true,
+        }
+    }
+
+    public componentDidMount() {
+        this.getData();
+    }
+
+    public render() {
+        let contents = this.state.loading
+            ? <Loader />
+            : this.renderGroupsBets();
+
+        return (
+            <div>
+                {contents}
+            </div>
+        );
+    }
+
+    private async getData() {
+        const groups = await getApi().getGroupBetsForUsers(this.props.users);
+        this.setState({ groups: groups, loading: false });
+    }
+
+    private renderGroupsBets() {
+        return (
+            <Table className="table table-striped opacity-table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        {userBets.getKeys().map((userId) => {
+                            return (<th key={userId}>{this.props.users?.find(u => u.id === userId)?.userName}</th>)
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {matches.map((match, index) => (
+                        <tr key={match.id}>
+                            <MatchBetRow match={match} bets={this.getBetsRow(userBets, match)} isReadOnly={!!this.props.users} />
+                        </tr>)
+                    )}
+                </tbody>
+            </Table>
+        );
+    }
+}
---
+++ Controllers/UsersBetsController.cs
@@ -0,0 +1,95 @@
+﻿namespace TipTournament2._0.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using TipTournament2._0.Data;
+    using TipTournament2._0.Models;
+
+    [ApiController]
+    [Authorize]
+    [Route("api/bets/users")]
+    public class UsersBetsController : Controller
+    {
+        private readonly IDbContextWrapper context;
+
+        [HttpPost]
+        public IActionResult GetBets([FromBody] string[] userIds)
+        {
+            var result = new Dictionary<string, MatchBet[]>();
+            foreach (var userId in userIds)
+            {
+                var bets = this.context.GetBetsForUser(userId);
+                result.Add(userId, bets.ToArray());
+            }
+            return new OkObjectResult(result);
+        }
+
+        [HttpPost("group")]
+        public IActionResult GetGroupBets([FromBody] string[] usersIds)
+        {
+            var result = new Dictionary<string, Dictionary<string, GroupBet>>();
+            foreach(var userId in usersIds)
+            {
+                var groupBets = this.context.GetGroupBetsForUser(userId);
+                var dict = new Dictionary<string, GroupBet>();
+                foreach(var groupBet in groupBets)
+                {
+                    dict.Add(groupBet.GroupId, groupBet);
+                }
+
+                result.Add(userId, dict);
+            }
+
+            return new OkObjectResult(result);
+        }
+
+        [HttpPost("delta")]
+        public IActionResult GetDeltaBets([FromBody] string[] usersIds, [FromQuery] TournamentStage stage)
+        {
+            var result = new Dictionary<string, Dictionary<string, DeltaBet>>();
+            foreach (var userId in usersIds)
+            {
+                var deltaBets = this.context.GetDeltaBetsForUserAndStage(stage, userId);
+                var dict = new Dictionary<string, DeltaBet>();
+                foreach (var deltaBet in deltaBets)
+                {
+                    dict.Add(deltaBet.Id, deltaBet);
+                }
+
+                result.Add(userId, dict);
+            }
+
+            return new OkObjectResult(result);
+        }
+
+        [HttpPost("lambda")]
+        public IActionResult GetLambdaBets([FromBody] string[] usersIds)
+        {
+            var result = new Dictionary<string, TopShooterBet>();
+            foreach (var userId in usersIds)
+            {
+                var shooterBet = this.context.GetShooterBet(userId);
+                result.Add(userId, shooterBet);
+            }
+
+            return new OkObjectResult(result);
+        }
+
+        [HttpPost("omikron")]
+        public IActionResult GetOmikronBets([FromBody] string[] usersIds, [FromQuery] bool isWinner)
+        {
+            var result = new Dictionary<string, SpecificTeamPlaceBet>();
+            foreach (var userId in usersIds)
+            {
+                var omikronBet = this.context.GetTeamPlaceBet(userId, isWinner);
+                result.Add(userId, omikronBet);
+            }
+
+            return new OkObjectResult(result);
+        }
+    }
+}
